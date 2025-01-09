f98b940f907b0dc7f4f8aeb75519372f8b141f98
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2025-01-09 22:22:06 +0500

    add all file in repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    FormsModule, 
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  rememberMe: boolean = false;
+  hide: boolean = true;
+
+
+  
+  togglePasswordVisibility() {
+    this.hide = !this.hide;
+  }
+
+  onSubmit() {
+    console.log("Email:", this.email);
+    console.log("Password:", this.password);
+    console.log("Remember Me:", this.rememberMe);
+  }
+}
