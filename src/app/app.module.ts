2d223d2c8f0b572ba6984a099df74b31b4495d34
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2025-01-10 15:27:44 +0500

    login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
+   AppComponent,
+   LoginComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    FormsModule, 
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule, 
   
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,23 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  email: string = '';
-  password: string = '';
-  rememberMe: boolean = false;
-  hide: boolean = true;
+  public passwordFieldType: string = 'password';
+  public eyeIcon: string = 'fa-eye';
 
-
-  
-  togglePasswordVisibility() {
-    this.hide = !this.hide;
-  }
-
-  onSubmit() {
-    console.log("Email:", this.email);
-    console.log("Password:", this.password);
-    console.log("Remember Me:", this.rememberMe);
+  public togglePasswordVisibility() {
+    this.passwordFieldType =
+      this.passwordFieldType === 'password' ? 'text' : 'password';
+    this.eyeIcon = this.eyeIcon === 'fa-eye' ? 'fa-eye-slash' : 'fa-eye';
   }
 }
