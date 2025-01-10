2accae40719054f23ded2145c9bb7367b9111710
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2025-01-11 04:29:36 +0500

    registration and verification component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { VerificationComponent } from './verification/verification.component';
 
 @NgModule({
   declarations: [
-   AppComponent,
-   LoginComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    VerificationComponent,
   ],
   imports: [
-    AppRoutingModule,
     BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
     FormsModule,
     MatFormFieldModule,
+    MatIconModule,
     MatInputModule,
-    MatButtonModule,
-    MatCheckboxModule, 
-  
+    MatSelectModule,
+    MatButtonModule,  
   ],
   providers: [],
   bootstrap: [AppComponent]
