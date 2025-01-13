85171f18219f6c78ee53b3a33930b6b87676fde5
Muhammad Afaq <afaq87626@gmail.com>
2025-01-13 05:34:26 +0500

    Resolved bugs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { HomeComponent } from './home/home.component';
+import { VerificationComponent } from './verification/verification.component';
 
 const routes: Routes = [
  {path:'', component:LoginComponent},
  {path:'login', component:LoginComponent},
  {path:'register', component:RegistrationComponent},
- {path:'home', component:HomeComponent}
+ {path:'home', component:HomeComponent},
+ {path:'verification', component: VerificationComponent}
 ]
 
 @NgModule({
